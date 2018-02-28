8843e076ef651753ee7759fffcaae4180a5502ee
trgree <trgree@foxmail.com>
2018-02-28 09:08:11 +0800

    add spark src

---
+++ spark/src/main/java/org/ace/DataSetTest.java
@@ -0,0 +1,40 @@
+package org.ace;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.*;
+
+/**
+ * Created by Liangsj on 2018/2/27.
+ */
+public class DataSetTest {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Java Spark SQL basic example")
+                .config("spark.master", "local")
+                .getOrCreate();
+        JavaRDD<Person> peopleRDD = spark.read()
+                .textFile("hdfs://192.168.5.150:8020/tmp/sparktest/people.txt")
+                .javaRDD()
+                .map(line -> {
+                    String[] parts = line.split(",");
+                    Person person = new Person();
+                    person.setName(parts[0]);
+                    person.setAge(Integer.parseInt(parts[1].trim()));
+                    return person;
+                });
+
+        Dataset<Row> peopleDF = spark.createDataFrame(peopleRDD, Person.class);
+        peopleDF.createOrReplaceTempView("people");
+
+        Dataset<Row> teenagersDF = spark.sql("SELECT name FROM people WHERE age BETWEEN 13 AND 25");
+
+        Encoder<String> stringEncoder = Encoders.STRING();
+        Dataset<String> teenagerNamesByIndexDF = teenagersDF.map(
+                (MapFunction<Row, String>) row -> "Name: " + row.getString(0),
+                stringEncoder);
+        teenagerNamesByIndexDF.show();
+
+    }
+}
