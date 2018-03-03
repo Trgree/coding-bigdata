f4b70f9fed8635ba25d518ddb04713318957e9ca
L <trgree@foxmail.com>
2018-03-03 20:44:58 +0800

    add zookeeper-DistributeLock

--- zookeeper/src/main/java/org/ace/distributelock/DistributeLock.java
+++ zookeeper/src/main/java/org/ace/distributelock/DistributeLock.java
@@ -13,13 +13,14 @@ import java.util.concurrent.locks.Lock;
 
 /**
  * 分布式锁实现
+ * 多个客户端请求乐，只有一个能获取锁，其它的按顺序等待，直到上一个锁释放
  *
  *  基于ZooKeeper分布式锁的流程：
  * 1。在zookeeper指定节点（locks）下创建临时顺序节点node_n   n会自增
  * 2。获取locks下所有子节点children
  * 3。对子节点按节点自增序号从小到大排序
- * 4。判断本节点是不是第一个子节点（第一个生成结点），若是，则获取锁；若不是，则监听比该节点小的那个节点的删除事件
- * 5。若监听事件生效，则回到第二步重新进行判断，直到获取到锁
+ * 4。判断本节点是不是第一个子节点（第一个生成结点），若是，则获取锁；若不是，则监听比该节点小的所有节点的删除事件
+ * 5。等到若所有比该节点小的所有节点都不在，获到锁
  * @author L
  * @date 2018/3/3
  */
@@ -43,6 +44,11 @@ public class DistributeLock  implements Lock,Watcher {
     // 等待锁，即上一结点
     private String waitLock;
 
+    // 等待锁，即前面的所有结点
+    private List<String> waitLocks;
+
+    private String lockPrefix;
+
 
     public DistributeLock(String hosts, String lockName)  {
         try {
@@ -52,6 +58,7 @@ public class DistributeLock  implements Lock,Watcher {
             zk = new ZooKeeper(hosts, SESSION_TIMEOUT, this);
             countDownLatch.await();
             this.lockName = lockName;
+            this.lockPrefix = lockName + "_lock_";
             Stat stat = zk.exists(ROOT_LOCK, false);
             if(stat == null) {
                 zk.create(ROOT_LOCK, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
@@ -76,36 +83,18 @@ public class DistributeLock  implements Lock,Watcher {
         if(this.tryLock()) {
             System.out.printf("%s 获得锁 %s\n", Thread.currentThread().getName(),currentLock );
         } else {
-            this.waitForLock(waitLock, COUNT_DOWN_AWAIT_TIME, TimeUnit.MILLISECONDS);
+            this.waitForAllPreLock(waitLocks, COUNT_DOWN_AWAIT_TIME, TimeUnit.MILLISECONDS);
         }
     }
 
     @Override
     public boolean tryLock() {
-        String lockPrefix = lockName + "_lock_";
+
         try {
             // 创建临时顺序结点
             currentLock = zk.create(ROOT_LOCK + "/" + lockPrefix, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
             System.out.println(Thread.currentThread().getName() + " 创建 currentLock = " + currentLock);
-            List<String> children = zk.getChildren(ROOT_LOCK, false);
-            // 取得所有lockname的结点
-            List<String> lockNodes = new ArrayList<>();
-            children.stream().forEach(n -> {
-                if(n.startsWith(lockPrefix)){
-                    lockNodes.add(n);
-                }
-            });
-            System.out.printf("%s 的所有锁结点：%s\n", lockName, lockNodes);
-
-            // 排序，如果当前创建结点为最小结点（即第一个创建），则认为获取当前锁
-            Collections.sort(lockNodes);
-            if((currentLock).equals(ROOT_LOCK + "/" + lockNodes.get(0))){
-                return true;
-            }
-
-            // 如果没有获得锁，则获取上一个结点,作为等待锁
-            String currentNode = currentLock.substring(currentLock.lastIndexOf("/") + 1);
-            waitLock = lockNodes.get(Collections.binarySearch(lockNodes, currentNode) - 1);
+            return isMinNode();
         } catch (KeeperException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -115,34 +104,73 @@ public class DistributeLock  implements Lock,Watcher {
         return false;
     }
 
+    /**
+     * 所有锁结点中，是否是最的那个
+     * @return
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    private boolean isMinNode() throws KeeperException, InterruptedException {
+        List<String> children = zk.getChildren(ROOT_LOCK, false);
+        // 取得所有lockname的结点
+        List<String> lockNodes = new ArrayList<>();
+        children.stream().forEach(n -> {
+            if(n.startsWith(lockPrefix)){
+                lockNodes.add(n);
+            }
+        });
+        System.out.printf("%s 的所有锁结点：%s\n", lockName, lockNodes);
+
+        // 排序，如果当前创建结点为最小结点（即第一个创建），则认为获取当前锁
+        Collections.sort(lockNodes);
+        if((currentLock).equals(ROOT_LOCK + "/" + lockNodes.get(0))){
+            return true;
+        }
+
+        // 如果没有获得锁，则获取上一个结点,作为等待锁
+        String currentNode = currentLock.substring(currentLock.lastIndexOf("/") + 1);
+        waitLock = lockNodes.get(Collections.binarySearch(lockNodes, currentNode) - 1);
+        waitLocks = lockNodes.subList(0, Collections.binarySearch(lockNodes, currentNode));
+        return false;
+    }
+
     @Override
     public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
         if(this.tryLock()) {
             System.out.printf("%s 获得锁 %s\n", Thread.currentThread().getName(),currentLock );
             return true;
         }
-        return waitForLock(waitLock, time, unit);
+        return waitForAllPreLock(waitLocks, time, unit);
 
     }
 
-    private boolean waitForLock(String waitLock, long time, TimeUnit unit)  {
+    private void waitForLock(String waitLock, long time, TimeUnit unit)  {
         try {
             Stat stat = zk.exists(ROOT_LOCK + "/" + waitLock, true);//
             if(stat != null) {
-                System.out.printf("%s 等待锁 %s\n" , Thread.currentThread().getName() ,waitLock);
+                System.out.printf("%s 等待锁 %s释放 \n" , Thread.currentThread().getName() ,waitLock);
                 countDownLatch = new CountDownLatch(1);
                 // 计数等待，若等到前一个节点消失，则precess中进行countDown，停止等待，获取锁
                 countDownLatch.await(time, unit);
                 countDownLatch = null;
-                System.out.println(Thread.currentThread().getName() + " 等到了锁");
+                System.out.printf("%s 锁释放\n" , waitLock);
             }
-            return true;
         } catch (KeeperException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
 
+    private boolean waitForAllPreLock(List<String> waitLocks, long time, TimeUnit unit) {
+        if(waitLocks.isEmpty()) {
+            return true;
+        }
+        System.out.printf("%s 要等待的锁一共有： %s\n" , Thread.currentThread().getName() ,waitLocks);
+        for(int i=waitLocks.size() - 1; i>=0;i--){
+            waitForLock(waitLocks.get(i), time, unit);
+        }
+        System.out.println(Thread.currentThread().getName() + " 等到了锁");
         return true;
     }
 
